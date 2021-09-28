de9340c13ac452bf5783758644b1d5875ca13576
inbear-evan <inbear49@gmail.com>
2021-09-29 02:05:01 +0900

    CompassExample
    CompassExample

---
+++ UnityCompass/Assets/BasicCompass/Scripts/Compass.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Compass : MonoBehaviour
+{
+    public Transform playerTransform;
+    Vector3 dir;
+
+    private void Update()
+    {
+        dir.z = playerTransform.eulerAngles.y;
+        transform.localEulerAngles = dir;
+    }
+
+
+}
